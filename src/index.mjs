c70eb443dd2f586c2461cb33def564c5adaff02d
Ali Raza <alirazakhalid1129@gmail.com>
2024-08-23 11:39:59 +0500

    added get requests

--- src/index.mjs
+++ src/index.mjs
@@ -1,1 +1,33 @@
-console.log("ali");
+import express from "express";
+import { posts } from "./utils/constants.mjs";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  console.log(req.method, "-", req.originalUrl);
+
+  res.status(200).send({ msg: "made by ALI RAZA @Ali1raz" });
+});
+
+app.get("/posts", (req, res) => {
+  console.log(req.method, "-", req.originalUrl);
+  res.status(200).send(posts);
+});
+
+app.get("posts/:id", (req, res) => {
+  console.log(req.method, "-", req.originalUrl);
+  const parsedId = parseInt(req.params.id);
+  if (isNaN(parsedId))
+    return res.status(400).send({ msg: "invlid ID, bad request" });
+
+  const findPostIndex = posts.find((post) => post.id === parsedId);
+
+  if (findPostIndex) res.status(200).send(findPostIndex);
+  else res.status(404).send({ msg: "not found" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on PORT ${PORT}`);
+});
---
+++ src/utils/constants.mjs
@@ -0,0 +1,20 @@
+export const posts = [
+  {
+    id: 1,
+    title: "My First Blog Post",
+    content: "This is the content of my first blog post.",
+    category: "Technology",
+    tags: ["Tech", "Programming"],
+    createdAt: "2021-09-01T12:00:00Z",
+    updatedAt: "2021-09-01T12:00:00Z",
+  },
+  {
+    id: 2,
+    title: "My Second Blog Post",
+    content: "This is the content of my second blog post.",
+    category: "Technology",
+    tags: ["Tech", "Programming"],
+    createdAt: "2021-09-01T12:30:00Z",
+    updatedAt: "2021-09-01T12:30:00Z",
+  },
+];
